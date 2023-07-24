d33934d0060fa248407ca7881251c5113b5c25fd
Josef Kopriva <jkopriva@redhat.com>
2023-07-24 14:34:55 +0200

    Small fixes for mage

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -240,9 +240,16 @@ func (ci CI) TestE2E() error {
 
 func RunE2ETests() error {
 	cwd, _ := os.Getwd()
+	labelsToSkip := "!upgrade-create && !upgrade-verify && !upgrade-cleanup"
+	labelFilter := os.Getenv("$E2E_TEST_SUITE_LABEL")
+	if labelFilter == "" {
+		labelFilter = labelsToSkip
+	} else {
+		labelFilter = labelFilter + " && " + labelsToSkip
+	}
 
 	// added --output-interceptor-mode=none to mitigate RHTAPBUGS-34
-	return sh.RunV("ginkgo", "-p", "-vv", "--output-interceptor-mode=none", "--timeout=90m", fmt.Sprintf("--output-dir=%s", artifactDir), "--junit-report=e2e-report.xml", "--label-filter=$E2E_TEST_SUITE_LABEL", "./cmd", "--", fmt.Sprintf("--config-suites=%s/tests/e2e-demos/config/default.yaml", cwd), "--generate-rppreproc-report=true", fmt.Sprintf("--rp-preproc-dir=%s", artifactDir))
+	return sh.RunV("ginkgo", "-p", "--output-interceptor-mode=none", "--timeout=90m", fmt.Sprintf("--output-dir=%s", artifactDir), "--junit-report=e2e-report.xml", "--label-filter="+labelFilter, "./cmd", "--", fmt.Sprintf("--config-suites=%s/tests/e2e-demos/config/default.yaml", cwd), "--generate-rppreproc-report=true", fmt.Sprintf("--rp-preproc-dir=%s", artifactDir))
 }
 
 func PreflightChecks() error {
